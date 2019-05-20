16143b2e1491bebc3a27553d4411536d71702a9d
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-05-20 12:27:40 +0200

    Arreglo

--- src/manualsqlite/BorrarCampo.java
+++ src/manualsqlite/BorrarCampo.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -32,6 +34,12 @@ public class BorrarCampo {
  
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }finally{
+            try {
+                conne.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(BorrarCampo.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
     }
